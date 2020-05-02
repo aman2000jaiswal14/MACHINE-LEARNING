f827f08208374b0f70616d63759ba05bf6a31382
AMAN JAISWAL <aman2000jaiswal14@gmail.com>
2020-05-02 15:45:16 +0530

    MEAN MEDIAN STD EXAMPLES

--- MLPractical26.py
+++ MLPractical26.py
@@ -7,3 +7,4 @@ print("Mean of x =", m_x)
 print("x.mean() = ",x.mean() )         # internal method if we need to use same array to store mean
 print("np.median(x) =",np.median(x))   # median has only external function (not have internal function)
 print("x.std() = ",x.std())
+
